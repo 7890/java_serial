c222409701ddbca3e7ba1683efb09578c9691402
Thomas Brand <tom@trellis.ch>
2016-02-28 16:35:46 +0100

    use JavaOSC jar

--- make.sh
+++ make.sh
@@ -32,36 +32,11 @@ checkAvail()
 }
 
 #========================================================================
-compile_java_osc()
-{
-	echo "building JavaOSC library (com.illposed.osc)"
-	echo "==========================================="
-	cp "$archive"/JavaOSC-master.zip "$build"
-	cd "$build"
-	unzip -q JavaOSC-master.zip
-	cd "$DIR"
-
-	cp "$archive"/JavaOSC_mod/OSCPort.java "$build"/JavaOSC-master/modules/core/src/main/java/com/illposed/osc/
-	cp "$archive"/JavaOSC_mod/OSCPortIn.java "$build"/JavaOSC-master/modules/core/src/main/java/com/illposed/osc/
-	cp "$archive"/JavaOSC_mod/OSCPortOut.java "$build"/JavaOSC-master/modules/core/src/main/java/com/illposed/osc/
-
-	cp "$archive"/JavaOSC_mod/AbstractOSCPacket.java "$build"/JavaOSC-master/modules/core/src/main/java/com/illposed/osc/
-	cp "$archive"/JavaOSC_mod/OSCByteArrayToJavaConverter.java "$build"/JavaOSC-master/modules/core/src/main/java/com/illposed/osc/utility/
-
-	PREF="$build"/JavaOSC-master/modules/core/src/main/java
-
-	echo "compiling files in $PREF to directory $build ..."
-
-	find "$PREF/com/illposed/osc/" -name *.java > "$TMPFILE"
-	$JAVAC -source $jsource -target $jtarget -classpath "$PREF" -sourcepath "$PREF" -d "$build" @"$TMPFILE"
-}
-
-#========================================================================
 compile_serial_reader()
 {
 	echo "building serial reader"
 	echo "======================"
-	$JAVAC -source $jsource -target $jtarget -classpath "$build":"$lib"/jssc-2.8.0.jar -sourcepath "$src" -d "$build" "$src"/*.java
+	$JAVAC -classpath "$build":"$lib"/jssc-2.8.0.jar:"$lib"/JavaOSC_1456673189.jar -sourcepath "$src" -d "$build" "$src"/*.java
 }
 
 #========================================================================
@@ -86,9 +61,11 @@ mkdir -p "$build"
 rm -rf "$build"/*
 
 #create_javadoc
-compile_java_osc
 compile_serial_reader
 
 echo "done."
-echo "list serial ports: java -cp _build:lib/jssc-2.8.0.jar ListSerialPorts"
-echo "use serial reader: java -cp _build:lib/jssc-2.8.0.jar SerialReader <serial portname> (baudrate)"
+echo "list serial ports: java -cp "$build":"$lib"/jssc-2.8.0.jar ListSerialPorts"
+echo ""
+echo "use serial reader: java -cp "$build":"$lib"/jssc-2.8.0.jar SerialReader <serial portname> (baudrate)"
+echo ""
+echo "use serial reader osc: java -cp "$build":"$lib"/jssc-2.8.0.jar:"$lib"/JavaOSC_1456673189.jar SerialReaderOSC <serial portname> (baudrate)"
