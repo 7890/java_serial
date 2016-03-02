7dd61f594f31fcc076957c1963405906cf71b400
Thomas Brand <tom@trellis.ch>
2016-03-02 02:23:53 +0100

    add error hints if no ports are found (mainly permission, linux only)

--- src/ListSerialPorts.java
+++ src/ListSerialPorts.java
@@ -9,5 +9,15 @@ public class ListSerialPorts
 		{
 			System.out.println(portNames[i]);
 		}
+		if(portNames.length<1)
+		{
+			System.err.println("no ports found.");
+			System.err.println("on linux:");
+			System.err.println("  ls -l /dev/ #look for tty* devices.");
+			System.err.println("check permissions, group membership");
+			System.err.println("if a plugged-in device port is still not shown:");
+			System.err.println("  sudo chmod 666 /dev/ttyXXX #as a temporary test");
+			System.err.println("  sudo chown :$USER /dev/ttyACM0 #alternative");
+		}
 	}
 }
