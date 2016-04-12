839435ea31b3ba0c00c2314053e91de5031eec1b
Thomas Brand <tom@trellis.ch>
2016-04-12 22:48:58 +0200

    add quality check, do not set system time until 'good data' available. minor mods.

--- src/SerialHookGNSSTime.java
+++ src/SerialHookGNSSTime.java
@@ -7,6 +7,7 @@ class SerialHookGNSSTime implements SerialHookInterface
 {
 	private NMEA n;
 	private GPSPosition pos;
+	private boolean all_done=false;
 
 	//this file is loaded if found in current directory
 	private String propertiesFileUri="SerialHookGNSSTime.properties";
@@ -15,6 +16,9 @@ class SerialHookGNSSTime implements SerialHookInterface
 	// /C time		/C date (omitted)
 	public String set_system_time_command="cmd /C nircmdc.exe elevate cmd /C time %"; //HH:MM:SS
 	public float seconds_to_next_full_second=0.9f;
+	public int minimal_sat_count=4;
+	public int minimal_quality=1;
+	public int seconds_offset_to_utc=0;
 	//===end configurable parameters
 
 //========================================================================
@@ -44,16 +48,29 @@ class SerialHookGNSSTime implements SerialHookInterface
 	public void serialData(String data)
 	{
 //		System.err.println("hook: serial data");
+		if(all_done){return;}
 
 		String[] lines = data.split("\n");
 		for(int i=0;i<lines.length;i++)
 		{
 //			System.err.println(lines[i]);
 			pos=n.parseLine(lines[i]);
+			if(pos==null || pos.quality<minimal_quality || pos.sat_in_use<minimal_sat_count)
+			{
+				System.err.println("Received data is not good enough.");
+				if(pos!=null)
+				{
+					System.err.println("last sentence: "+pos.last_sentence_type+" quality: "+pos.quality+" satellites: "+pos.sat_in_use+" time: "+pos.time);
+				}
+				//break; //dismiss all other lines
+				continue;
+			}
+
 			if(pos!=null&& pos.last_sentence_type.equals("RMC"))
 			{
 				//if....
-				System.out.print("GNSS TIME "+pos.time);
+				System.out.print("GNSS TIME "+pos.time +" quality: "+pos.quality+" satellites: "+pos.sat_in_use+" time: "+pos.time);
+
 				if(pos.time % 1.0f == 0)
 				{
 					System.out.println(" FOUND .0 TIME");
@@ -66,20 +83,23 @@ class SerialHookGNSSTime implements SerialHookInterface
 						///to set next second, respecting offset
 						String strTimeToSet=
 							DTime.timeCFromMillis(
-								DTime.millisFrom_HHMMSSpSSS(
-									DTime.formatTimeLeadingZeros(pos.time+1.0f)
-								)
+								DTime.millisFrom_HHMMSSpSSS(DTime.formatTimeLeadingZeros(pos.time))
+								+ 1000 * (1 + seconds_offset_to_utc)
 							);
 
-						System.err.print("NEXT TICK AT "+strTimeToSet);
+						System.err.print("NEXT TICK AT "+strTimeToSet+"  (including offset "+seconds_offset_to_utc+")");
 
 						Thread.sleep((int)(seconds_to_next_full_second*1000));
 						System.err.println(" .");
 
-						Runtime.getRuntime().exec(set_system_time_command.replaceAll("%",strTimeToSet)); // hh:mm:ss
+						System.err.println("Setting system time...");
+						Process p=Runtime.getRuntime().exec(set_system_time_command.replaceAll("%",strTimeToSet)); // hh:mm:ss
+						p.waitFor();
 
 						//drop other lines until next call
 						///break;
+						System.err.println("done.");
+						all_done=true;
 						System.exit(0);
 					}
 					catch(Exception e)
